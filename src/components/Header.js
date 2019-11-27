68d7d7adffe444b9d481a47e25786a3a23ccdf58
André Dargains <andre.dargains@gmail.com>
2019-11-27 13:08:24 +0000

    UPD: header control links;

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => (
   <header>
     LUGAR LIVRE
+    <ul>
+      <li>
+        <Link to="/">normal screen</Link>
+      </li>
+      <li>
+        <Link to="/accept">accept screen</Link>
+      </li>
+      <li>
+        <Link to="/refuse">refuse screen</Link>
+      </li>
+    </ul>
   </header>
 );
 
