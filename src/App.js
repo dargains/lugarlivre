57d4a544cd2e193c8a4ebfe3cb1bb921635e7013
André Dargains <andre.dargains@gmail.com>
2019-12-02 17:45:47 +0000

    RFC: commented header;

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Header from './components/Header'
+// import Header from './components/Header'
 import Home from './pages/Home'
 import Accept from './pages/Accept';
 import Refuse from './pages/Refuse';
