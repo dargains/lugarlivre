096897bced55b43e270773faa5a61ddef5d2d286
André Dargains <andre.dargains@gmail.com>
2019-12-02 11:29:18 +0000

    ADD: title component;

---
+++ src/components/Title.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const styles = {
+  color: '#484D5A',
+  fontSize: '20px',
+  fontWeight: 'bold',
+  lineGeight: '27px',
+  textAlign: 'center',
+  textTransform: 'uppercase'
+}
+
+function Title() {
+  return (
+    <h1 style={styles}>
+      Lugar Livre
+    </h1>
+  )
+}
+
+export default Title
