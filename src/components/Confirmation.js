eaf2cc7f5659d279a9300e07caa9ef2d765cf205
André Dargains <andre.dargains@gmail.com>
2019-12-05 11:13:41 +0000

    UPD: confirmation colors;

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import moment from 'moment'
 
+import ButtonContainer from '../components/ButtonContainer'
 import Button from '../components/Button'
 import AltButton from './AltButton'
 
-const Confirmation = ({ currentOwner, chosenBeliever, startDate, endDate, handleConfirmation, handleBack }) => {
+const Confirmation = ({ currentOwner, chosenBeliever, startDate, endDate, believers, handleConfirmation, handleBack }) => {
 
   const isSame = moment(startDate).isSame(endDate)
   const startDateString = moment(startDate).isSame(endDate, 'year') ? moment(startDate).isSame(endDate, 'month') ? moment(startDate).format('DD') : moment(startDate).format('DD [de] MMMM') : moment(startDate).format('DD [de] MMMM [de] YYYY');
   const endDateString = moment(endDate).format("DD [de] MMMM [de] YYYY");
+  const backColor = () => believers.indexOf(chosenBeliever) + 1
 
   return (
-    <article>
+    <article style={{ backgroundColor: `var(--m-0${backColor()})` }}>
       <h2>Confirme a escolha</h2>
       <p>{currentOwner?.name} quer oferecer o lugar a {chosenBeliever.name} {isSame ? `em ${startDate.format("DD [de] MMMM [de] YYYY")}` : `de ${startDateString} a ${endDateString}`}</p>
-      <Button handleClick={handleConfirmation}>Confirmar</Button>
-      <AltButton handleClick={handleBack}>Voltar</AltButton>
+      <ButtonContainer>
+        <Button white color={backColor()} handleClick={handleConfirmation}>Confirmar</Button>
+        <AltButton handleClick={handleBack}>Voltar</AltButton>
+      </ButtonContainer>
     </article>
   )
 }
