0f401c6595b8503424491b3d0ddb3aa7ce735d93
André Dargains <andre.dargains@gmail.com>
2019-12-06 12:25:46 +0000

    ADD: themes;

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { ThemeProvider } from 'styled-components'
+import { lightTheme, darkTheme } from './themes'
 
 // import Header from './components/Header'
 import Home from './pages/Home'
-import Accept from './pages/Accept';
-import Refuse from './pages/Refuse';
-import Error from './pages/Error';
+import Accept from './pages/Accept'
+import Refuse from './pages/Refuse'
+import Error from './pages/Error'
 
 const App = () => {
 
   return (
-    <Router>
-      <main className="App">
-        {/* <Header /> */}
-        <Switch>
-          <Route path="/accept">
-            <Accept />
-          </Route>
-          <Route path="/refuse">
-            <Refuse />
-          </Route>
-          <Route path="/error">
-            <Error />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-
-        </Switch>
-
-      </main>
-    </Router>
+    <ThemeProvider theme={lightTheme}>
+      <Router>
+        <main className="App">
+          {/* <Header /> */}
+          <Switch>
+            <Route path="/accept">
+              <Accept />
+            </Route>
+            <Route path="/refuse">
+              <Refuse />
+            </Route>
+            <Route path="/error">
+              <Error />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/themes.js
@@ -0,0 +1,9 @@
+export const lightTheme = {
+  background: 'var(--neu-01)',
+  text: 'var(--neu-06)',
+}
+
+export const darkTheme = {
+  background: 'var(--neu-01)',
+  text: 'var(--neu-06)',
+}
