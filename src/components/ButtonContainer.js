c88a60f19d27d12d07566c93e48fa87028f8b3cf
André Dargains <andre.dargains@gmail.com>
2019-12-09 09:23:12 +0000

    ADD: buttonContainer;

--- src/components/ButtonContainer.js
+++ src/components/ButtonContainer.js
@@ -16,6 +16,9 @@ const Container = styled.div`
   right: 0;
   width: 100%;
   text-align: center;
+  opacity: 0;
+  transform: scale(0.7);
+  animation: fadeIn .2s cubic-bezier(0.175, 0.885, 0.32, 1.4) .8s forwards;
 `;
 
 export default ButtonContainer
