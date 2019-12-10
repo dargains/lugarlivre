3887543386d868c8ce4d1124bcedf6d243926c84
André Dargains <andre.dargains@gmail.com>
2019-12-10 14:32:05 +0000

    UPD: calendar styling;

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,11 +3,12 @@ import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import ReactAutocomplete from 'react-autocomplete';
 import { DayPickerRangeController } from 'react-dates';
-import isAfterDay from 'react-dates/lib/utils/isAfterDay';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 import moment from 'moment';
 import 'moment/locale/pt'
+
+import isInclusivelyAfterDay from '../helpers/isInclusivelyAfterDay';
 
 import Container from '../components/Container'
 import Title from '../components/Title'
@@ -45,6 +46,7 @@ const Intro = ({ owners, currentOwner, handleOwnerChange, startDate, endDate, ha
     if (currentOwner.name) {
       document.querySelector('input').classList.remove('error')
       setShowError(false)
+      setCalendarOpen(false)
       handleNext()
     } else {
       document.querySelector('input').classList.add('error')
@@ -104,29 +106,35 @@ const Intro = ({ owners, currentOwner, handleOwnerChange, startDate, endDate, ha
       </AutoCompleteContainer>
 
       {
-        calendarOpened
-          ? <>
+        calendarOpened &&
+        <CalendarContainer>
+          <div className="mask" onClick={() => setCalendarOpen(false)} />
+          <div className="info">
             <DayPickerRangeController
               startDate={startDate}
               endDate={endDate}
               numberOfMonths={1}
               minimumNights={0}
               minDate={moment()}
-              maxDate={moment().add(10, 'days')}
+              maxDate={null}
               onDatesChange={handleDateChange}
               focusedInput={focusedInput}
               onFocusChange={newFocus => { newFocus === 'endDate' ? setFocus('endDate') : setFocus('startDate') }}
+              noBorder
+              withPortal={false}
               enableOutsideDays={false}
-              isDayBlocked={day => isAfterDay(day, 'days')}
+              isOutsideRange={(day) => !isInclusivelyAfterDay(day, moment())
+                || isInclusivelyAfterDay(day, moment().add(1, 'months'))}
             />
-            <Button handleClick={goToNext}>Continuar</Button>
-          </>
-          : <ButtonContainer>
-            <Button primary handleClick={() => setDate('today')} isActive={isToday}>hoje</Button>
-            <Button handleClick={() => setDate('tomorrow')} isActive={isTomorrow}>amanhã</Button>
-            <AltButton handleClick={() => setCalendarOpen(true)} icon="date_range" />
-          </ButtonContainer>
+            <Button white color="var(--neu-06)" handleClick={goToNext} >Continuar</Button>
+          </div>
+        </CalendarContainer>
       }
+      <ButtonContainer>
+        <Button primary handleClick={() => setDate('today')} isActive={isToday}>hoje</Button>
+        <Button handleClick={() => setDate('tomorrow')} isActive={isTomorrow}>amanhã</Button>
+        <AltButton handleClick={() => setCalendarOpen(true)} icon="date_range" />
+      </ButtonContainer>
       <Error className={showError ? 'show' : ''}>Falta dizer quem és</Error>
     </Container>
   );
@@ -191,6 +199,77 @@ const AutoCompleteContainer = styled.article`
   .border_bl:after {
     bottom: 0;
     left: 0;
+  }
+`;
+const CalendarContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: -28px;
+  width: calc(100% + 48px);
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  z-index: 1;
+  backdrop-filter: blur(2px);
+  .mask {
+    background-color: var(--neu-05);
+    opacity: 0.7;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    animation: fadeInPartial 0.3s ease forwards;
+  }
+  .info {
+    background-color: var(--neu-01);
+    border-radius: 30px;
+    animation: fadeInGrow 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.275) forwards;
+  }
+  .DayPicker {
+    margin-top: 30px;
+  }
+  .CalendarDay__default {
+    color: var(--neu-03);
+    border: 0;
+    vertical-align: middle;
+  }
+  .DayPicker_weekHeader_li {
+    color: var(--neu-04);
+  }
+  .CalendarDay__blocked_out_of_range,
+  .CalendarDay__blocked_out_of_range:active,
+  .CalendarDay__blocked_out_of_range:hover {
+    color: var(--neu-02);
+  }
+  .CalendarMonth_caption {
+    color: var(--neu-04);
+    font-size: 1em;
+    strong {
+      font-weight: 400;
+    }
+  }
+  .DayPickerNavigation_button__default,
+  .DayPickerNavigation_button__default:focus,
+  .DayPickerNavigation_button__default:hover {
+    border: 0;
+  }
+  .CalendarDay__selected,
+  .CalendarDay__selected_span,
+  .CalendarDay__selected:active,
+  .CalendarDay__selected:hover {
+    background: var(--neu-03);
+    border-color: var(--neu-03);
+    color: var(--neu-01);
+    border: 0;
+  }
+  .DayPickerNavigation_svg__horizontal {
+    fill: var(--neu-03);
+  }
+  .DayPickerNavigation_svg__disabled {
+    fill: var(--neu-02);
   }
 `;
 
