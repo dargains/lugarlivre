237a1db92ab392dd02afb01216c9523612eeb905
André Dargains <andre.dargains@gmail.com>
2019-12-10 14:31:56 +0000

    UPD: final screen styling;

--- src/components/Final.js
+++ src/components/Final.js
@@ -15,12 +15,19 @@ const Final = () => {
   return (
     <Container backColor="var(--m-02)">
       <ImageContainer className="scene">
-        <img src={cloud1} alt="cloud" data-depth="0.4" />
-        <img src={cloud2} alt="cloud" data-depth="0.8" />
-        <img src={cloud3} alt="cloud" data-depth="0.2" />
+        <figure data-depth="0.4">
+          <img src={cloud1} alt="cloud" />
+        </figure>
+        <figure data-depth="0.8">
+          <img src={cloud2} alt="cloud" />
+        </figure>
+        <figure data-depth="0.2">
+          <img src={cloud3} alt="cloud" />
+
+        </figure>
       </ImageContainer>
       <Box>
-        <Subtitle>Obrigado!</Subtitle>
+        <Subtitle>Obrigado</Subtitle>
         <Body>Acabaste de ganhar um lugar no céu</Body>
       </Box>
     </Container >
@@ -39,29 +46,42 @@ const Box = styled.article`
   height: 100%;
   width: 100%;
   margin: 0 auto;
-  `;
+  opacity: 0;
+    transform: translate3d(0,80px,0);
+  animation: floatIn 1.2s cubic-bezier(0.165, 0.84, 0.44, 1) .3s forwards;
+`;
+
 const ImageContainer = styled.div`
   position: absolute;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
-  img {
+  figure {
     
     &:nth-of-type(1) {
       width: 135px;
       top: 20% !important;
       left: 20% !important;
+      img {
+        animation: cloud1 6s infinite alternate ease-in-out;
+      }
     }
     &:nth-of-type(2) {
       width: 184px;
       top: 50% !important;
       left: 70% !important;
+      img {
+        animation: cloud2 4s infinite alternate ease-in-out;
+      }  
     }
     &:nth-of-type(3) {
       width: 56px;
       top: 70% !important;
       left: 20% !important;
+      img {
+        animation: cloud3 8s infinite alternate ease-in-out;
+      }
     }
   }
 `;
