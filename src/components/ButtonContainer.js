c97133cfb8bb45a02df19620f176b2894cefdac1
André Dargains <andre.dargains@gmail.com>
2019-12-10 14:31:00 +0000

    UPD: buttonContainer animation;

--- src/components/ButtonContainer.js
+++ src/components/ButtonContainer.js
@@ -18,7 +18,7 @@ const Container = styled.div`
   text-align: center;
   opacity: 0;
   transform: scale(0.7);
-  animation: fadeIn .2s cubic-bezier(0.175, 0.885, 0.32, 1.4) .8s forwards;
+  animation: fadeInGrow .2s cubic-bezier(0.175, 0.885, 0.32, 1.4) .8s forwards;
 `;
 
 export default ButtonContainer
