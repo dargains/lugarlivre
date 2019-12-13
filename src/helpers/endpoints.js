91c4d3de1873cbf6ed2c18058000265faeed567e
André Dargains <andre.dargains@gmail.com>
2019-12-13 15:03:07 +0000

    UPD: endpoint emailEndpoint;

--- src/helpers/endpoints.js
+++ src/helpers/endpoints.js
@@ -2,6 +2,6 @@ export const baseUrl = 'https://lugarlivre.azurewebsites.net'
 
 export const dataEndpoint = 'https://myeverydayapps.com/public/_/items'
 
-export const emailEndpoint = 'https://functionstestlogs.azurewebsites.net/api/SendEmail?code=1k9alxFBsZFlF0mHUlV/1wG58CLO0Xo79aoAZOh4af1p1SWi3fkCgQ=='
+export const emailEndpoint = 'https://functionappf6.azurewebsites.net/api/LugarLivreEmail?code=KwZtLhan1FKec/MmPaoqOEDzFLM0ULZsaBeTqktq89CANTpY/1/lQw=='
 
 export const smsEndpoint = 'https://api.twilio.com/2010-04-01/Accounts/AC43e03b4c673010868acc52eb0e44d08f/Messages.json'
